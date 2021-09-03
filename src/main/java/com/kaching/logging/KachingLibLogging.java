cad9231d21b0767157ab76365a973cb74acee792
DuongVu98 <duongvutung1998@gmail.com>
2021-09-03 21:43:02 +0700

    add ci script

---
+++ src/main/java/com/kaching/logging/KachingLibLogging.java
@@ -0,0 +1,12 @@
+package com.kaching.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KachingLibLogging {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KachingLibLogging.class, args);
+  }
+}
